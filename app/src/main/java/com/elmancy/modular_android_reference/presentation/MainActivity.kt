21e32f357da48d2e03e0f665e737c5bd5665b932
El Mancy <abdelrahman.elmancy@onebanck.eg>
2026-01-12 15:10:33 +0200

    add CoreActivity.kt - CoreFragment.kt

--- app/src/main/java/com/elmancy/modular_android_reference/presentation/MainActivity.kt
+++ app/src/main/java/com/elmancy/modular_android_reference/presentation/MainActivity.kt
@@ -1,17 +1,11 @@
 package com.elmancy.modular_android_reference.presentation
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import com.elmancy.modular_android_reference.R
 import com.elmancy.modular_android_reference.databinding.ActivityMainBinding
-import com.elmancy.presentation.CoreActivity
+import com.elmancy.presentation.activity.CoreActivity
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
-
 
 class MainActivity : CoreActivity<ActivityMainBinding>(
     ActivityMainBinding::inflate
